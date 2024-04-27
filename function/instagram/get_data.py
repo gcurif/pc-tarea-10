f40a0281c439b704780e74c0fe1f490e4b62ee77
pastitayika <diosdelsexo@pafcat.net>
2024-04-26 20:58:50 -0400

    creacion de los modelos en data

---
+++ function/instagram/get_data.py
@@ -0,0 +1,11 @@
+from models.instagram.dto import NewUser, showNewUser
+
+#nuevos usuarios
+NewUserSaniel = NewUser("Sanielgay123", "sanielVSpene123", "56", "sanielwenopalpene@gmail.com", "transexual")
+NewUserHarold = NewUser("haroldweko", "haroldchamorromaricon123", "53", "haroldweko@gmail.com", "lesbiana")
+NewUserTata = NewUser("tatatraidor", "tatapagamemasporfa", "31, 33 o 75", "tatatraidor@gmail.com", "gay")
+
+
+NewUserSaniel.showNewUser()
+NewUserHarold.showNewUser()
+NewUserTata.showNewUser()
